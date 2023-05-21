b363f5e37bb2c783ca076abc544a8042c66e0716
Pedro Fernandes <pedrofdnn@gmail.com>
2023-05-21 19:30:14 -0300

    função de adcionar ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import { useState } from "react";
+import ToDoList from "./assets/Components";
 
 export default function App() {
-  function TodaLista() {
-    const [todas, setTodas] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-
-    const addtodas = () => {
-      if (inputValue !== "") {
-        setTodas([...todas, inputValue]);
-        setInputValue("");
-      }
-    };
-  }
-
   return (
     <div>
-      <h1>Lista de Tarefas</h1>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button>Adcionar Tarefa</button>
-      <ul>{}</ul>
+      <ToDoList />
     </div>
   );
 }
